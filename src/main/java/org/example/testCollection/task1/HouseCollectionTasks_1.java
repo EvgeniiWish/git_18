fb8bedfe45571985104695fc243d322b58090313
EvgeniiWish <145448713+EvgeniiWish@users.noreply.github.com>
2024-05-10 11:48:17 +0200

    Students 5 task

--- src/main/java/org/example/testCollection/task1/HouseCollectionTasks_1.java
+++ src/main/java/org/example/testCollection/task1/HouseCollectionTasks_1.java
@@ -7,13 +7,25 @@ public class HouseCollectionTasks_1 {
     // Задание 1: Вернуть список всех квартир во всех домах (List)
     public static List<Flat> getAllFlats(List<House> houses) {
         List<Flat> allFlats = new ArrayList<>();
-
+        for (House house : houses) {
+            allFlats.addAll(house.flats);
+        }
         return allFlats;
+    }
+    public static List<Flat> getAllFlats1(List<House> houses){
+        return houses.stream().flatMap(house ->house.flats.stream()).toList();
     }
 
     // Задание 2: Вернуть множество всех уникальных цветов, используемых в комнатах квартир (Set)
     public static Set<Color> getUniqueRoomColors(List<House> houses) {
         Set<Color> colors = new HashSet<>();
+        for (House house : houses) {
+            for (Flat flat : house.flats) {
+                for (Room room : flat.roomList) {
+                    colors.add(room.color);
+                }
+            }
+        }
 
         return colors;
     }
@@ -21,22 +33,40 @@ public class HouseCollectionTasks_1 {
     // Задание 3: Вернуть карту, ключами которой являются номера этажей, а значениями - количество квартир на этом этаже (Map)
     public static Map<Integer, Integer> getFlatsCountByFloor(House houses) {
         Map<Integer, Integer> floorMap = new HashMap<>();
-
-        return floorMap;
+        Map<Integer, Integer> floorToFlatCount = null;
+        for (Flat flat : houses.flats) {
+           if (floorToFlatCount.containsKey(flat.floor)) {
+               int count = floorToFlatCount.get(flat.floor) + 1;
+               floorToFlatCount.put(flat.floor, count);
+           }else {
+               floorToFlatCount.put(flat.floor,1);
+           }
+       }
+        return floorToFlatCount;
     }
 
     // Задание 4: Используя очередь, вернуть список номеров квартир в порядке, в котором они появляются, начиная с первого подъезда (Queue)
     public static List<Integer> getFlatNumbersInOrder(House houses) {
-        Queue<Integer> queue = new LinkedList<>();
-        List<Integer> orderedFlatNumbers = new ArrayList<>();
-
+//        Queue<Integer> queue = new LinkedList<>();
+//        List<Integer> orderedFlatNumbers = new ArrayList<>();
+Queue<Flat> flatQueue = new PriorityQueue<>(Comparator.comparingInt(o -> o.flatnumber));
+flatQueue.addAll(houses.flats);
+List<Integer> orderedFlatNumbers = new ArrayList<>();
+while (!flatQueue.isEmpty()){
+    orderedFlatNumbers.add(flatQueue.poll().flatnumber);
+}
         return orderedFlatNumbers;
     }
 
     // Задание 5: Используя стек, вернуть список всех квартир в обратном порядке от последнего дома к первому (Stack)
     public static List<Flat> getFlatsInReverseOrder(House houses) {
         Stack<Flat> stack = new Stack<>();
+        houses.flats.forEach(flat -> stack.push(flat));
+        for (Flat flat : houses.flats){
+            stack.push(flat);
+        }
         List<Flat> reversedFlats = new ArrayList<>();
+
 
         return reversedFlats;
     }
